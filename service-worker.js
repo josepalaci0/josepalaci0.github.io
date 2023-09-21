4c17cd20b0ec0a691b997bf00351866f90d3cb16
Jose Gregorio Palacio Sanchez <92130994+josepalaci0@users.noreply.github.com>
2023-09-20 21:26:49 -0500

    cinfig pwa

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+// service-worker.js
+
+const CACHE_NAME = 'v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/script.js',
+    '/icon.png',
+    '/assets/'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    if (event.request.url.startsWith(self.location.origin)) {
+        // Esto asegura que el Service Worker solo maneje solicitudes relacionadas con tu dominio.
+        event.respondWith(
+            caches.match(event.request).then(response => {
+                return response || fetch(event.request);
+            })
+        );
+    }
+});
