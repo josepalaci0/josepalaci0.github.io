c2e318fb990970be875968837a70acbda3632986
Jose Gregorio Palacio Sanchez <92130994+josepalaci0@users.noreply.github.com>
2023-09-23 09:00:08 -0500

    config prettier

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,31 @@
+/** @format */
+
 let isTrue = false;
 let originalBackgroundColor; // Declarar originalBackgroundColor como variable global
 
 const App = {
-    mostrarSeccion: (seccion) => {
-        // Ocultar todas las secciones
-        const secciones = ['acercademi', 'inicio', 'servicios', 'blog', 'foro', 'proyectos','documentos', 'contacto'];
-        secciones.forEach((seccionId) => {
-            const elemento = document.getElementById(seccionId);
-            if (elemento) {
-                elemento.style.display = 'none';
-            }
-        });
-        // Mostrar la sección seleccionada
-        const seccionActual = document.getElementById(seccion);
-        if (seccionActual) {
-            seccionActual.style.display = 'block';
-           
-        }
-       
-    },
-    manejarmensaje: (tipo) => {
-        // Obtenemos el elemento <input>
-        const inputElement = document.querySelector('input[type="text"]');
-        
-        // Obtenemos el valor del input
-        const mensaje = inputElement.value;
-    
-        // Construimos el mensaje basado en el tipo (Frontend, Backend, Full Stack)
-        const mensajeCompleto = `${mensaje}`;
-        
-        // Obtenemos el elemento <code>
-        const codeElement = document.querySelector('code.hljs');
-    
-        // Modificamos el contenido del elemento <code> con el mensaje
-        codeElement.textContent = `
-    const fn = (mensaje) => {
-      console.log('${mensajeCompleto}', 'estoy Programando en ${tipo}');
-    }
-        `.trim();
-    
-        // Volvemos a resaltar la sintaxis
-        hljs.highlightAll();
-      },
-      
+   mostrarSeccion: (seccion) => {
+      // Ocultar todas las secciones
+      const secciones = [
+         "acercademi",
+         "inicio",
+         "servicios",
+         "blog",
+         "foro",
+         "proyectos",
+         "documentos",
+         "contacto",
+      ];
+      secciones.forEach((seccionId) => {
+         const elemento = document.getElementById(seccionId);
+         if (elemento) {
+            elemento.style.display = "none";
+         }
+      });
+      // Mostrar la sección seleccionada
+      const seccionActual = document.getElementById(seccion);
+      if (seccionActual) {
+         seccionActual.style.display = "block";
+      }
+   },
 };
-
-
-
-
-
-
-
-
--- script.js
+++ script.js
@@ -1,100 +1,82 @@
 // ---------Responsive-navbar-active-animation-----------
-function test(){
-	var tabsNewAnim = $('#navbarSupportedContent');
-	var selectorNewAnim = $('#navbarSupportedContent').find('li').length;
-	var activeItemNewAnim = tabsNewAnim.find('.active');
-	var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
-	var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
-	var itemPosNewAnimTop = activeItemNewAnim.position();
-	var itemPosNewAnimLeft = activeItemNewAnim.position();
-	$(".hori-selector").css({
-		"top":itemPosNewAnimTop.top + "px", 
-		"left":itemPosNewAnimLeft.left + "px",
-		"height": activeWidthNewAnimHeight + "px",
-		"width": activeWidthNewAnimWidth + "px"
-	});
+function test() {
+    var tabsNewAnim = $('#navbarSupportedContent')
+    var selectorNewAnim = $('#navbarSupportedContent').find('li').length
+    var activeItemNewAnim = tabsNewAnim.find('.active')
+    var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight()
+    var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth()
+    var itemPosNewAnimTop = activeItemNewAnim.position()
+    var itemPosNewAnimLeft = activeItemNewAnim.position()
+    $('.hori-selector').css({
+        top: itemPosNewAnimTop.top + 'px',
+        left: itemPosNewAnimLeft.left + 'px',
+        height: activeWidthNewAnimHeight + 'px',
+        width: activeWidthNewAnimWidth + 'px',
+    })
 
-	$("#navbarSupportedContent").on("click","li",function(e){
-		$('#navbarSupportedContent ul li').removeClass("active");
-		$(this).addClass('active');
-		var activeWidthNewAnimHeight = $(this).innerHeight();
-		var activeWidthNewAnimWidth = $(this).innerWidth();
-		var itemPosNewAnimTop = $(this).position();
-		var itemPosNewAnimLeft = $(this).position();
-		$(".hori-selector").css({
-			"top":itemPosNewAnimTop.top + "px", 
-			"left":itemPosNewAnimLeft.left + "px",
-			"height": activeWidthNewAnimHeight + "px",
-			"width": activeWidthNewAnimWidth + "px"
-		});
-	});
+    $('#navbarSupportedContent').on('click', 'li', function (e) {
+        $('#navbarSupportedContent ul li').removeClass('active')
+        $(this).addClass('active')
+        var activeWidthNewAnimHeight = $(this).innerHeight()
+        var activeWidthNewAnimWidth = $(this).innerWidth()
+        var itemPosNewAnimTop = $(this).position()
+        var itemPosNewAnimLeft = $(this).position()
+        $('.hori-selector').css({
+            top: itemPosNewAnimTop.top + 'px',
+            left: itemPosNewAnimLeft.left + 'px',
+            height: activeWidthNewAnimHeight + 'px',
+            width: activeWidthNewAnimWidth + 'px',
+        })
+    })
 }
-$(document).ready(function(){
-	setTimeout(function(){ test(); });
-});
-$(window).on('resize', function(){
-	setTimeout(function(){ test(); }, 500);
-});
-$(".navbar-toggler").click(function(){
-	$(".navbar-collapse").slideToggle(300);
-	setTimeout(function(){ test(); });
-});
-
-
+$(document).ready(function () {
+    setTimeout(function () {
+        test()
+    })
+})
+$(window).on('resize', function () {
+    setTimeout(function () {
+        test()
+    }, 500)
+})
+$('.navbar-toggler').click(function () {
+    $('.navbar-collapse').slideToggle(300)
+    setTimeout(function () {
+        test()
+    })
+})
 
 // --------------add active class-on another-page move----------
-jQuery(document).ready(function($){
-	// Get current path and find target link
-	var path = window.location.pathname.split("/").pop();
+jQuery(document).ready(function ($) {
+    // Get current path and find target link
+    var path = window.location.pathname.split('/').pop()
 
-	// Account for home page with empty path
-	if ( path == '' ) {
-		path = 'index.html';
-	}
+    // Account for home page with empty path
+    if (path == '') {
+        path = 'index.html'
+    }
 
-	var target = $('#navbarSupportedContent ul li a[href="'+path+'"]');
-	// Add active class to target link
-	target.parent().addClass('active');
-});
+    var target = $('#navbarSupportedContent ul li a[href="' + path + '"]')
+    // Add active class to target link
+    target.parent().addClass('active')
+})
 
+document.addEventListener('DOMContentLoaded', function () {
+    var navLinks = document.querySelectorAll('.nav-link')
 
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
+            // Obtén el ancho de la ventana del navegador
+            var windowWidth =
+                window.innerWidth || document.documentElement.clientWidth
 
-
-// Add active class on another page linked
-// ==========================================
-// $(window).on('load',function () {
-//     var current = location.pathname;
-//     console.log(current);
-//     $('#navbarSupportedContent ul li a').each(function(){
-//         var $this = $(this);
-//         // if the current path is like this link, make it active
-//         if($this.attr('href').indexOf(current) !== -1){
-//             $this.parent().addClass('active');
-//             $this.parents('.menu-submenu').addClass('show-dropdown');
-//             $this.parents('.menu-submenu').parent().addClass('active');
-//         }else{
-//             $this.parent().removeClass('active');
-//         }
-//     })
-// });
-
-document.addEventListener("DOMContentLoaded", function () {
-	var navLinks = document.querySelectorAll(".nav-link");
-  
-	navLinks.forEach(function (link) {
-	  link.addEventListener("click", function () {
-		// Obtén el ancho de la ventana del navegador
-		var windowWidth = window.innerWidth || document.documentElement.clientWidth;
-  
-		// Si es un dispositivo móvil (ancho de ventana menor a 992px), cierra el menú desplegable
-		if (windowWidth < 992) {
-		  var navbarToggler = document.querySelector(".navbar-toggler");
-		  if (navbarToggler.classList.contains("collapsed") === false) {
-			navbarToggler.click();
-		  }
-		}
-	  });
-	});
-  });
-  
-
+            // Si es un dispositivo móvil (ancho de ventana menor a 992px), cierra el menú desplegable
+            if (windowWidth < 992) {
+                var navbarToggler = document.querySelector('.navbar-toggler')
+                if (navbarToggler.classList.contains('collapsed') === false) {
+                    navbarToggler.click()
+                }
+            }
+        })
+    })
+})
